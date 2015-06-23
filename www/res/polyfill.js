8394eed5105d268584315edbeec752f58a45bc5d
Samuel Grundman <samuel@yodas.ws>
2015-06-23 00:46:12 -0400

    Fix typo in log10 shim
    Closes #55

--- www/res/polyfill.js
+++ www/res/polyfill.js
@@ -9,5 +9,5 @@ String.prototype.trim=function(chars){return this.replace(new RegExp('^['+(chars
 Number.isInteger=Number.isInteger||function(a){return typeof a==='number'&&isFinite(a)&&Math.abs(a)<Math.pow(2,53)-1&&Math.floor(a)===a};
 Number.parseFloat=Number.parseFloat||parseFloat;
 Number.parseInt=Number.parseInt||parseInt;
-Math.log10=Math.log10||function(x){return Math.log(x)/Math.log(x)};
+Math.log10=Math.log10||function(x){return Math.log(x)/Math.log(10)};
 Math.sign=Math.sign||function(x){x=+x;if(x===0||isNaN(x))return x;return x>0?1:-1};
